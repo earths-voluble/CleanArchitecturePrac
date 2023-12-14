86342e9e0538ee77270ce6b26241b2d15e269cf8
Paul Lee <earths-voluble.0g@icloud.com>
2023-12-14 17:16:01 +0900

    [initial] Initial Commit of Swift Clean Architecture Practice

---
+++ CleanArchitecturePrac/CleanArchitecturePracApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitecturePracApp.swift
+//  CleanArchitecturePrac
+//
+//  Created by 이보한 on 2023/12/14.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitecturePracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
