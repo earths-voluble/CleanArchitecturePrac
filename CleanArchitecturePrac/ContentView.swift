f8cdb6c4aefadccf978256286f37bdbb51719b55
Paul Lee <earths-voluble.0g@icloud.com>
2023-12-14 17:28:22 +0900

    [docs] Added annotations

--- CleanArchitecturePrac/ContentView.swift
+++ CleanArchitecturePrac/ContentView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+// 이 데이터 모델은 엔티티와 UI 관련 정보가 혼재되어 있다.
+// 클린 아키텍쳐에서, 엔티티는 어떠한 UI 의존성을 가져서는 안된다.
 struct Pokemon: Identifiable, Decodable {
     let id: Int
     let name: String
+    // 이미지 URL 생성같은 UI문제는 분리되어야 한다.
+    // 이것은 UI레이어나 ViewModel에서 수행될 수 있다.
     var imageUrl: URL? {
         return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(id).png")
     }
@@ -33,6 +37,8 @@ struct ContentView: View {
                 PokemonView(pokemon: pokemon)
             }
         }
+        // 뷰에서의 직접적인 Data fetching은 클린 아키텍쳐에 위배된다.
+        // Data fetching은 뷰모델 혹은 use case같은 분리된 레이어로 이동되어야 한다.
         .task {
             do {
                 pokemonList = try await getPokemonList()
@@ -41,7 +47,8 @@ struct ContentView: View {
             }
         }
     }
-    
+    // 이 함수는 data fetching, parsing, tranforming 작업들을 결합하고 있다.
+    // 이러한 기능들은 단일 책임 원칙에 따라 분리되어야 한다.
     func getPokemonList() async throws -> [Pokemon] {
         guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=50") else {
             throw URLError(.badURL)
---
+++ CleanArchitecturePrac/Translate.swift
@@ -0,0 +1,8 @@
+//
+//  Translate.swift
+//  CleanArchitecturePrac
+//
+//  Created by 이보한 on 2023/12/14.
+//
+
+import Foundation
